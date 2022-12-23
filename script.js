8d09611f8cff04746a886796d11c5d0c639264c5
tanglero4 <tiffanyanglero94@hotmail.com>
2022-12-23 17:52:43 -0500

    set main variables

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//Assugn variables
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = [1,2,3,4,5,6,7,8,9,0]
+var characters = ['!','@','#','$','^','&','*']
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +13,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
