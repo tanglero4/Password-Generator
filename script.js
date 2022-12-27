89932a3fef773e2db850ea7d0b755e065600d31e
tanglero4 <tiffanyanglero94@hotmail.com>
2022-12-27 16:53:49 -0500

    Update character values

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Assugn variables
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = [1,2,3,4,5,6,7,8,9,0]
-var characters = ['!','@','#','$','^','&','*']
 
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Sz"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const specialChar = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "-", "_", "/", ".", ",", "'", ";", ":", "[","]","{", "}", "?", "<", ">", "|",]
+
+var characterAmount1 = window.prompt(`Choose between 8 - 128 characters`);
+var upperCaseQuestion = window.prompt(`Do you want upper case letters?`);
+var lowerCaseQuestion = window.prompt(`Do you want lower case letters?`);
+var numbersQuestion = window.prompt(`Do you want numbers?`);
+var specialCharactersQuestion = window.prompt(`Do you want special characters?`);
 
 // Write password to the #password input
+
+//start the function
+
+
 function generatePassword() {
 
-  return;
 }
+generatePassword();
+
+
 
 function writePassword() {
   var password = generatePassword();
