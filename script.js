657232f39f2f28f55d680d7c206ead40ccd5bc82
tanglero4 <tiffanyanglero94@hotmail.com>
2022-12-27 17:01:53 -0500

    Added inital question

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var generateBtn = document.querySelector("#generate");
 
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "Sz"];
 const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specialChar = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "-", "_", "/", ".", ",", "'", ";", ":", "[","]","{", "}", "?", "<", ">", "|",]
 
-var characterAmount1 = window.prompt(`Choose between 8 - 128 characters`);
+
 var upperCaseQuestion = window.prompt(`Do you want upper case letters?`);
 var lowerCaseQuestion = window.prompt(`Do you want lower case letters?`);
 var numbersQuestion = window.prompt(`Do you want numbers?`);
@@ -19,6 +19,16 @@ var specialCharactersQuestion = window.prompt(`Do you want special characters?`)
 
 
 function generatePassword() {
+ var addonOptions = []
+  var finalPassword = ""
+  var passwordLength = prompt ("How long do you want your password to be? ")
+
+  if (passwordLength <= 8 && passwordLength >= 128) {
+  
+     alert ("Please choose a number between 8 and 128 characters.")
+     generatePassword();
+     
+    }
 
 }
 generatePassword();
